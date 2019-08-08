5be2a7f14e3d2addd81a8a23e3815c821e0ed8c5
Accusoft <support@accusoft.com>
2019-08-08 17:57:46 +0000

    Second beta release

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e # stop on error
-set +x # do not echo commands
-
-cd PrizmDocRestClient
-
-# Do a release build of the DLL we want to test and publish.
-dotnet build --configuration Release
